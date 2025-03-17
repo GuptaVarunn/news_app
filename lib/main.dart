9b8fb3dc5dad1c91cb74bea5c01c331d4f3ed8ca
GuptaVarunnn <2022.varun.gupta@ves.ac.in>
2025-03-17 14:34:46 +0530

    homepage navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// ignore: unused_import
 import 'screens/home_screen.dart';
 import 'firebase_options.dart'; // Ensure this file is correctly generated
 
@@ -34,15 +33,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FirebaseCheckScreen extends StatelessWidget {
+class FirebaseCheckScreen extends StatefulWidget {
+  @override
+  _FirebaseCheckScreenState createState() => _FirebaseCheckScreenState();
+}
+
+class _FirebaseCheckScreenState extends State<FirebaseCheckScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => NewsHomePage()),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Firebase Test')),
       body: Center(
         child: Text(
-          '✅ Firebase is working!',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          '✅ Firebase is working! Redirecting...',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
       ),
     );
